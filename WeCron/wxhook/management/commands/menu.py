01f57488c3d96bc21e763bdb49ab644ad46d0d1c
poly <mcx_221@foxmail.com>
2016-10-14 08:16:54 -0400

    Add menu command

---
+++ WeCron/wxhook/management/commands/menu.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from __future__ import unicode_literals, absolute_import
+
+import json
+from django.core.management import BaseCommand
+from common import wechat_client
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('new_menu', nargs='*')
+
+    def handle(self, *args, **options):
+        if not options.get('new_menu'):
+            self.stdout.write(
+                    json.dumps(
+                        wechat_client.menu.get(),
+                        ensure_ascii=False))
+        else:
+            new_menu = json.loads(options['new_menu'][0])
+            self.stdout.write(
+                    json.dumps(wechat_client.menu.create(new_menu),
+                    indent=2,
+                    ensure_ascii=False))
