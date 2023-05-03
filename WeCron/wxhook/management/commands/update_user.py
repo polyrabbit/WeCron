63aa847be63330a7df0b52f75ef2334d37cce920
polyrabbit <mcx_221@foxmail.com>
2023-05-04 00:24:10 +0800

    Defer deleting unsubscribed users & reminds

--- WeCron/wxhook/management/commands/update_user.py
+++ WeCron/wxhook/management/commands/update_user.py
@@ -6,6 +6,8 @@ import random
 
 from django.core.management import BaseCommand
 from django.contrib.auth import get_user_model
+from django.db import connection
+from django.db.utils import DatabaseError
 from common import wechat_client
 from wechatpy import WeChatClientException
 
@@ -15,6 +17,27 @@ BATCH_SIZE = 90
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        self.check_subscribtion()
+        self.clear_reminds()
+        self.clear_user()
+
+    def clear_reminds(self):
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute('delete from time_remind r where not exists(select * from "user" where r.owner_id="user".openid and "user".subscribe=true)')
+                print 'deleted %d reminds' % cursor.rowcount
+            except DatabaseError as e:
+                print e
+
+    def clear_user(self):
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute('delete from "user" where subscribe=false')
+                print 'deleted %d users' % cursor.rowcount
+            except DatabaseError as e:
+                print e
+
+    def check_subscribtion(self):
         start = time.time()
         user_manager = get_user_model().objects
         total_size = user_manager.count()
